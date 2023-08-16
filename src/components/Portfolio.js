cdd4b1d2980a139df50c1a0306c5e72172009d1a
omarm <omarmousa@outlook.com>
2023-08-16 14:11:31 -0700

    Updated the resume and a few other QOL

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,7 @@ export default function Portfolio({ resumeData }) {
     <section id="portfolio">
       <div className="row">
         <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
+          <h1>Check Out Some of My Works. Right Click and select open in new tab or Middle Click, Currently trying to fix the bug</h1>
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
             {resumeData.portfolio &&
               resumeData.portfolio.map((item) => {
